ad4e2c9b14bdf6c49f04736fef94dad1d0135a3b
Leonel <leonelaf12@gmail.com>
2019-04-22 16:54:07 -0400

    finished header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <div className = 'header-title'>
+            <h1>Lambda School<span> @lambda School 22 Apr </span></h1>
+            <p>Lets learn React by building simple interfaces with components. Dont try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
