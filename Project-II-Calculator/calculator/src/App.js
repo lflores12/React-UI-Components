c33f94a9253cb28099648c6eb5c322351a341bed
Leonel <leonelaf12@gmail.com>
2019-04-23 17:21:55 -0400

    finished all components of project. seems to be looking fine

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ButtonContainer from './components/ButtonComponents/ButtonContainer';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
 
 const App = () => {
       return (
       <div className = 'calc-container'>
         <CalculatorDisplay />
+        <ButtonContainer>
+          <ActionButton className = 'twoButton-text' text = 'clear' />
+          <NumberButton bgColor = 'red-button' text = '/' />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor = 'num-button' text = '7'/>
+          <NumberButton bgColor = 'num-button' text = '8'/>
+          <NumberButton bgColor = 'num-button' text = '9'/>
+          <NumberButton bgColor = 'red-button' text = 'X' />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor = 'num-button' text = '4'/>
+          <NumberButton bgColor = 'num-button' text = '5'/>
+          <NumberButton bgColor = 'num-button' text = '6'/>
+          <NumberButton bgColor = 'red-button' text = '-' />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton bgColor = 'num-button' text = '1'/>
+          <NumberButton bgColor = 'num-button' text = '2'/>
+          <NumberButton bgColor = 'num-button' text = '3'/>
+          <NumberButton bgColor = 'red-button' text = '+' />
+        </ButtonContainer>
+        <ButtonContainer>
+          <ActionButton className = 'twoButton-text' text = '0' />
+          <NumberButton bgColor = 'red-button' text = '=' />
+        </ButtonContainer>
         </div>
       );
     };
